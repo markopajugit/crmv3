27c929e62f6cf9d3d6e2976a8eb70cc7416b821a
Marko Paju <markopaju92@gmail.com>
2025-11-11 14:11:02 +0200

    style update

--- resources/views/pdf/wisorgroup.blade.php
+++ resources/views/pdf/wisorgroup.blade.php
@@ -4,124 +4,38 @@
     <meta charset="UTF-8">
     <title>Invoice</title>
     <style>
-        body {
-            font-family: Helvetica, sans-serif;
-            font-size: 12px;
-            color: #000000;
-        }
-
-        .container {
-            width: 892px;
-            margin: 0 auto;
-            background-size: cover;
-            padding: 30px;
-            box-sizing: border-box;
-        }
-
-        .header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .header .logo {
-            width: 50%;
-        }
-
-        .header .dates {
-            width: 50%;
-            text-align: right;
-        }
-
-        .invoice-number {
-            font-size: 20px;
-            font-weight: bold;
-            margin-top: 30px;
-        }
-
-        .address {
-            display: flex;
-            justify-content: space-between;
-            margin-top: 30px;
-        }
-
-        .recipient {
-            width: 50%;
-        }
-
-        .account-details {
-            width: 50%;
-            text-align: right;
-        }
-
-        table {
-            width: 100%;
-            margin-top: 30px;
-            border-collapse: collapse;
-        }
-
-        table th,
-        table td {
-            padding: 5px;
-            text-align: left;
-        }
-
-        table th {
-            border-top: 1px solid black;
-            color: #808080;
-            font-weight: normal;
-        }
-
-        table tbody{
-            border-bottom: 2px solid black;
-        }
-
-        .total {
-            margin-top: 30px;
-            text-align: right;
-            font-size: 16px;
-            font-weight: bold;
-        }
-
-        .footer {
-            margin-top: 30px;
-            font-size: 10px;
-            color: #808080;
-        }
-        p {
-            margin:3px 0;
-        }
+        {!! file_get_contents(resource_path('views/pdf/css/invoice.css')) !!}
     </style>
 </head>
 <body>
-<div style="margin-bottom: 20px;">
-    <div style="float: left; width: 50%;">
+<div class="invoice-header">
+    <div class="invoice-header-left">
         <img src="{{ asset('images/wisor-logo.jpg') }}" style="width:150px;" alt="logo">
     </div>
-    <div style="float: right; width: 50%; padding-top: 50px;">
-        <p style="font-size:16px;"><b>Advance payment invoice {{ $data['invoice']['number'] }}</b></p>
-        <div style="float: left; width: 35%;">
+    <div class="invoice-header-right">
+        <p class="invoice-title"><b>Advance payment invoice {{ $data['invoice']['number'] }}</b></p>
+        <div class="invoice-dates-left">
             <p>Date</p>
             <p>Due Date</p>
         </div>
-        <div style="float: right; width: 65%;">
+        <div class="invoice-dates-right">
             <p>{{ $data['invoice']['issue_date'] }}</p>
             <p>{{ $data['invoice']['payment_date'] }}</p>
         </div>
     </div>
-    <div style="clear: both;"></div>
+    <div class="invoice-header-clear"></div>
 </div>
 
 <div>
-    <div style="float: left; width: 50%;">
-        <p style="color:#808080;">Bill to:</p>
+    <div class="recipient">
+        <p class="invoice-bill-to">Bill to:</p>
         @if($data['invoice']['payer_name'])
-            <p style="font-size:14px; font-weight: bold;">{{$data['invoice']['payer_name']}}</p>
+            <p class="invoice-bill-to-name">{{$data['invoice']['payer_name']}}</p>
             @if(isset($data['invoice']['registry_code']))
                 <p>Reg code: {{ $data['invoice']['registry_code'] }}</p>
             @endif
         @else
-            <p style="font-size:14px; font-weight: bold;">{{ $data['company']['name'] }}</p>
+            <p class="invoice-bill-to-name">{{ $data['company']['name'] }}</p>
             @if(isset($data['invoice']['registry_code']))
                 <p>Reg code: {{ $data['invoice']['registry_code'] }}</p>
             @elseif(isset($data['company']['registry_code']))
@@ -162,26 +76,26 @@
         <p>{{ $data['company']['address_zip'] }}</p>
         <p>{{ $data['company']['address_dropdown'] }}</p>-->
     </div>
-    <div style="float: right; width: 50%; font-weight: bold;">
-        <p style="color:#808080;"><b>SEPA payment details</b></p>
-        <div style="float: left; width: 35%;">
+    <div class="invoice-payment-section">
+        <p class="invoice-payment-title"><b>SEPA payment details</b></p>
+        <div class="invoice-payment-left">
             <p>Recipient</p>
             <p>Account</p>
         </div>
-        <div style="float: right; width: 65%;">
-            <p>Wisor Group OÜ</p>
-            <p>LT123500010008545129</p>
+        <div class="invoice-payment-right">
+            <p>xxxxxxxxxxxx</p>
+            <p>xxxxxxxxxxxxxxxxxx</p>
         </div>
-        <div style="height:70px;"></div>
-        <p style="color:#808080;"><b>SWIFT/BIC payment details</b></p>
-        <div style="float: left; width: 35%;">
+        <div class="invoice-payment-spacer"></div>
+        <p class="invoice-payment-title"><b>SWIFT/BIC payment details</b></p>
+        <div class="invoice-payment-left">
             <p>Recipient</p>
             <p>Account</p>
             <p>SWIFT/BIC</p>
             <p>Bank</p>
             <p>Bank address</p>
         </div>
-        <div style="float: right; width: 65%;">
+        <div class="invoice-payment-right">
             <p>Wisor Group OÜ</p>
             <p>EE264204278619971400</p>
             <p>EKRDEE22</p>
@@ -189,16 +103,16 @@
             <p>Maakri 30, 15014 Tallinn, Estonia</p>
         </div>
     </div>
-    <div style="clear: both;"></div>
+    <div class="invoice-header-clear"></div>
 </div>
 
 <div style="margin-bottom: 30px;">
-    <table style="width: 100%; border-collapse: collapse;">
+    <table class="invoice-table">
         <thead>
         <tr>
-            <th style="text-align: left; padding: 5px;">Description</th>
-            <th style="text-align: left; padding: 5px;">Period</th>
-            <th style="text-align: right; padding: 5px;">Sum</th>
+            <th>Description</th>
+            <th>Period</th>
+            <th class="text-right">Sum</th>
         </tr>
         </thead>
         <tbody>
@@ -217,7 +131,7 @@
                             {{$service['pivot']['date_from']}} - {{$service['pivot']['date_to']}}
                         @endif
                     </td>
-                    <td style="text-align: right;">@if(!is_null($service['pivot']['cost'])){{ sprintf("%01.2f", $service['pivot']['cost']) }}€@endif</td>
+                    <td class="text-right">@if(!is_null($service['pivot']['cost'])){{ sprintf("%01.2f", $service['pivot']['cost']) }}€@endif</td>
                 </tr>
             @endif
         @endforeach
@@ -230,7 +144,7 @@
                             {{$service['pivot']['date_from']}} - {{$service['pivot']['date_to']}}
                         @endif
                     </td>
-                    <td style="text-align: right;">@if(!is_null($service['pivot']['cost'])){{ sprintf("%01.2f", $service['pivot']['cost']) }}€@endif</td>
+                    <td class="text-right">@if(!is_null($service['pivot']['cost'])){{ sprintf("%01.2f", $service['pivot']['cost']) }}€@endif</td>
                 </tr>
             @endif
         @endforeach
@@ -239,57 +153,57 @@
         @if($data['invoice']['vat'] == 20)
         <tr>
             <td></td>
-            <td style="padding: 5px;">Net sum</td>
-            <td style="text-align: right; padding: 5px;">{{ $data['totals']['sum'] }}€</td>
+            <td>Net sum</td>
+            <td class="text-right">{{ $data['totals']['sum'] }}€</td>
         </tr>
         <tr>
             <td></td>
-            <td style="padding: 5px; border-bottom: 1px solid black;">VAT 20%</td>
-            <td style="text-align: right; padding: 5px; border-bottom: 1px solid black;">{{ $data['totals']['vat'] }}€</td>
+            <td class="border-bottom">VAT 20%</td>
+            <td class="text-right border-bottom">{{ $data['totals']['vat'] }}€</td>
         </tr>
         @endif
         @if($data['invoice']['vat'] == 22)
             <tr>
                 <td></td>
-                <td style="padding: 5px;">Net sum</td>
-                <td style="text-align: right; padding: 5px;">{{ $data['totals']['sum'] }}€</td>
+                <td>Net sum</td>
+                <td class="text-right">{{ $data['totals']['sum'] }}€</td>
             </tr>
             <tr>
                 <td></td>
-                <td style="padding: 5px; border-bottom: 1px solid black;">VAT 22%</td>
-                <td style="text-align: right; padding: 5px; border-bottom: 1px solid black;">{{ $data['totals']['vat'] }}€</td>
+                <td class="border-bottom">VAT 22%</td>
+                <td class="text-right border-bottom">{{ $data['totals']['vat'] }}€</td>
             </tr>
         @endif
         @if($data['invoice']['vat'] == 24)
             <tr>
                 <td></td>
-                <td style="padding: 5px;">Net sum</td>
-                <td style="text-align: right; padding: 5px;">{{ $data['totals']['sum'] }}€</td>
+                <td>Net sum</td>
+                <td class="text-right">{{ $data['totals']['sum'] }}€</td>
             </tr>
             <tr>
                 <td></td>
-                <td style="padding: 5px; border-bottom: 1px solid black;">VAT 24%</td>
-                <td style="text-align: right; padding: 5px; border-bottom: 1px solid black;">{{ $data['totals']['vat'] }}€</td>
+                <td class="border-bottom">VAT 24%</td>
+                <td class="text-right border-bottom">{{ $data['totals']['vat'] }}€</td>
             </tr>
         @endif
-        <tr style="font-size: 16px; font-weight: bold;">
+        <tr class="total-row">
             <td></td>
-            <td style="padding: 5px;">Total</td>
-            <td style="text-align: right; padding: 5px;">{{ $data['totals']['sumwithvat'] }}€</td>
+            <td>Total</td>
+            <td class="text-right">{{ $data['totals']['sumwithvat'] }}€</td>
         </tr>
         </tfoot>
     </table>
 </div>
 
-<div>
-    <p style="text-align: center; margin-bottom: 15px;">VAT {{ $data['invoice']['vat'] }}% as per Estonian Value-added Tax Act §15 section 4 p 1.</p>
-    <p style="text-align: center; margin: 5px 0;"><b>On payment order please refer to "Invoice {{ $data['invoice']['number'] }}".</b></p>
-    <p style="text-align: center;">In case the invoice number is missing, we might not be able to connect your payment to your order.</p>
-    <p style="text-align: center;">By paying you agree with the terms and conditions of Wisor Group OÜ.</p>
+<div class="invoice-text-center">
+    <p class="margin-bottom">VAT {{ $data['invoice']['vat'] }}% as per Estonian Value-added Tax Act §15 section 4 p 1.</p>
+    <p><b>On payment order please refer to "Invoice {{ $data['invoice']['number'] }}".</b></p>
+    <p>In case the invoice number is missing, we might not be able to connect your payment to your order.</p>
+    <p>By paying you agree with the terms and conditions of Wisor Group OÜ.</p>
 </div>
 
-<div class="footer" style="color: #808080; border-top: 1px solid #808080; bottom:40px; position: absolute; width: 100%;">
-    <div style="float: left; width: 50%;">
+<div class="invoice-footer">
+    <div class="invoice-footer-left">
         <p>Wisor Group OÜ</p>
         <p>Address: Narva mnt 5, Tallinn 10117, Estonia</p>
         <p>
@@ -297,7 +211,7 @@
             conditions.
         </p>
     </div>
-    <div style="float: right; text-align:right; width: 50%;">
+    <div class="invoice-footer-right">
         <p>Reg no: 14818750</p>
         <p>VAT ID: EE102195839</p>
     </div>
